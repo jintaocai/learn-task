9b4bd855afb3ce34754103fb198c91ddbfcd64c6
justcurd <justcrud@163.com>
2019-04-19 15:54:23 +0800

    init

---
+++ src/main/java/com/justcrud/demo/learntask/LearnTaskApplication.java
@@ -0,0 +1,13 @@
+package com.justcrud.demo.learntask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTaskApplication.class, args);
+	}
+
+}
